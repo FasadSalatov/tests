fa33ef18885143580cd3cbc3614ac6dcf5230ffd
Fasad <salatiksama@gmail.com>
2024-08-12 12:06:22 +0300

    Create QRCodeDisplay.vue

---
+++ src/components/QRCodeDisplay.vue
@@ -0,0 +1,39 @@
+<!-- src/components/QRCodeDisplay.vue -->
+<template>
+  <div class="qr-container">
+    <canvas ref="qrCanvas"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import QRCode from "qrcode";
+
+const qrCanvas = ref(null);
+
+onMounted(() => {
+  QRCode.toCanvas(
+    qrCanvas.value,
+    "https://t.me/battle_iq_test_bot/battleiqtestapp",
+    function (error) {
+      if (error) console.error(error);
+      console.log("QR код успешно создан!");
+    }
+  );
+});
+</script>
+
+<style scoped>
+.qr-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* высота контейнера */
+  width: 100%; /* ширина контейнера */
+}
+
+canvas {
+  max-width: 100%; /* Ограничение ширины QR-кода */
+  max-height: 100%; /* Ограничение высоты QR-кода */
+}
+</style>
